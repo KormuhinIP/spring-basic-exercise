17b090fd9e663d53b14d288e1abb44c0f3424e0a
Maksim Leshukov <pacificrus63@mail.ru>
2018-09-25 17:31:02 +0400

    Add method remove

--- src/main/java/com/mycompany/spring_basic/exercise/api/CurrencyRepository.java
+++ src/main/java/com/mycompany/spring_basic/exercise/api/CurrencyRepository.java
@@ -5,6 +5,11 @@ import com.mycompany.spring_basic.exercise.entity.Currency;
 import java.util.List;
 
 public interface CurrencyRepository {
+
     Currency create(String symbol, boolean active);
+
     List<Currency> find(boolean active);
+
+    void remove(String currencyId);
+    
 }
